863403b4ba217547a2595d4da8ab20085907bf75
Loc Phan <lphan@openwt.com>
2020-08-11 21:42:36 +0700

    update package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, AfterViewChecked, AfterContentChecked, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, AfterContentChecked, ChangeDetectorRef, LOCALE_ID, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -10,11 +10,13 @@ export class AppComponent implements OnInit, AfterContentChecked {
   public languageCurrent;
   constructor(
     private cdref: ChangeDetectorRef,
-    private translateService: TranslateService) {
+    private translateService: TranslateService,
+    @Inject(LOCALE_ID) protected localeId: string) {
   }
   ngOnInit(): void {
     this.translateService.setDefaultLang('en');
     this.translateService.use('en');
+    console.log(this.localeId);
   }
   ngAfterContentChecked(): void {
     this.languageCurrent = this.translateService.currentLang;
