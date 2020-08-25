2e25e0ccde780ce17b7cd91807996c5efdc77a74
Loc Phan <lphan@openwt.com>
2020-08-25 18:43:48 +0700

    complete

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SecureInnerPagesGuard } from 'src/app/common/guard/secure-inner-pages.guard';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { VerifyEmailPageComponent } from './pages/verify-email-page/verify-email-page.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
     canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+    canActivate: [SecureInnerPagesGuard]
+  },
+  {
+    path: 'verify-email',
+    component: VerifyEmailPageComponent,
   },
   {
     path: '',
